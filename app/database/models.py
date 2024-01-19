f794741e172f807ed005db1fd6117419729e1796
okadapy <ozzajilon@gmail.com>
2024-01-19 23:26:22 +0300

    models ready to use!

--- app/database/models.py
+++ app/database/models.py
@@ -4,26 +4,35 @@ from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy import ForeignKey, func
 from typing import Optional, List
 
+
 class AsyncTable(DeclarativeBase, AsyncAttrs):
     pass
+
 
 class User(AsyncTable):
     __tablename__ = "fgis_bot_users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
     tg_id: Mapped[BigInteger] = mapped_column(BigInteger, index=True, unique=True)
     username: Mapped[Optional[String]] = mapped_column(String, nullable=True)
+
     first_name: Mapped[String] = mapped_column(String, nullable=False)
     last_name: Mapped[Optional[String]] = mapped_column(String, nullable=True)
+    phone: Mapped[String] = mapped_column(String, nullable=False)
+    requests: Mapped[List["Requests"]] = relationship(back_populates="")
+    created: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), nullable=False, default=func.now()
+    )
 
-    created: Mapped[DateTime] = mapped_column(DateTime(timezone=True), nullable=False, default=func.now())
-    requests: Mapped[List["Request"]] = relationship(back_populates="user")
 
-class Request(AsyncTable):
+class Requests(AsyncTable):
     __tablename__ = "fgis_bot_requests"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    request_type: Mapped[String] = mapped_column(String, nullable=False)
-    mit_id: Mapped[int] = mapped_column(index=True, nullable=False)
-    number: Mapped[int] = mapped_column(index=True, nullable=False)
-    
+    mit: Mapped[Optional[int]]
+    factory_number: Mapped[Optional[int]]
+    requested_by: Mapped[int] = relationship(ForeignKey("fgis_bot_users.id"))
+    created: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), nullable=False, default=func.now()
+    )
